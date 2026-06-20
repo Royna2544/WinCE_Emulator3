78540d53ff7d25cd1218f3bd3a09174e1c7b91de
Soo-Hwan Na <whiteshell2544@naver.com>
2026-06-20 13:44:13 +0900

    fix: preserve reload library flags

--- src/ce/kernel.rs
+++ src/ce/kernel.rs
@@ -1994,6 +1994,7 @@ impl CeKernel {
         {
             module.load_flags &= !DONT_RESOLVE_DLL_REFERENCES_FLAG;
         }
+        module.load_flags |= requested_flags & !NO_IMPORT_LOAD_FLAGS;
         Some(module.base)
     }
 
